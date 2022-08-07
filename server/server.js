5c6097422ee54b7705f3f94d4ea5482330861f03
Sudhanshu Shukla <sudhanshushukla@192.168.1.5>
2022-08-07 21:52:20 +0530

    updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,33 +1,57 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+//old way of connection
+const MONGO_URI =
+  "mongodb+srv://sudshukla:sud1234@cluster-graphqlfe.7a4bo5s.mongodb.net/?retryWrites=true&w=majority";
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error("You must provide a MongoLab URI");
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once("openUri", () => console.log("Connected to MongoLab instance."))
+  .on("error", (error) => console.log("Error connecting to MongoLab:", error));
+
+//Latest  connection config mongodb
+/* const uri =
+  "mongodb+srv://sudshukla:sud1234@cluster-graphqlfe.7a4bo5s.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
